222121ea08f8dc566d170395f88bc81669bd67dd
praisy pekeleke <praisypekeleke@gmail.com>
2026-05-05 17:13:32 +0100

    Additional Testings

--- ClassLibrary/clsDoctor.cs
+++ ClassLibrary/clsDoctor.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsDoctor
     {
+        public bool Available { get; set; }
+        public DateTime ContractDate { get; set; }
+        public int DoctorId { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string Street { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
