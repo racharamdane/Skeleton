fbd66c106453e9c7530d777ccb6ced4c812c5521
P2932328 <P2932328@my365.dmu.ac.uk>
2026-05-07 14:49:02 +0100

    simple presentation layer (patient)

--- AdminSystem/PatientDataEntry.aspx.cs
+++ AdminSystem/PatientDataEntry.aspx.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        // Create a new instance of clsPatient
+        ClsPatient APatient = new ClsPatient();
+        // Get the data entered by the user
+        APatient.FullName = txtPatientfullname.Text;
+        //Store the data in the session object
+        Session["APatient"] = APatient;
+        // capture the patient date of birth
+          APatient.DateAdded = Convert.ToDateTime(txtPatientdateofbirth.Text);
+        // capture the patient email
+        APatient.Email = txtPatientemail.Text;
+        // capture the patient gender
+        APatient.PatientGender = txtPatientgender.Text;
+        // capture the patient password
+         APatient.PatientPassword = txtPatientpassword.Text;
+        // capture the patient treatment status
+        APatient.Treatment = chkPatienttreatment.Checked;
+        //Navigate to the next page
+        Response.Redirect("PatientViewer.aspx");
     }
 }
--- AdminSystem/PatientViewer.aspx.cs
+++ AdminSystem/PatientViewer.aspx.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // create an instance of the patient class
+        ClsPatient APatient = new ClsPatient();
+        // get the data from the session object
+        APatient = (ClsPatient)Session["APatient"];
+        // display the patient data for this entry
+        Response.Write(APatient.FullName + "<br/>");
+        Response.Write(APatient.DateAdded + "<br/>");
+        Response.Write(APatient.Email + "<br/>");
+        Response.Write(APatient.PatientGender + "<br/>");
+        Response.Write(APatient.PatientPassword + "<br/>");
+        Response.Write(APatient.Treatment + "<br/>");
     }
 }
