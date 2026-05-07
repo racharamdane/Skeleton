821a2c9850bb89ab1bfc4afe88972e30e4e5b4d6
praisy pekeleke <praisypekeleke@gmail.com>
2026-05-07 14:50:26 +0100

    Adding a Simple Presentation Layer

--- AdminSystem/DoctorDataEntry.aspx.cs
+++ AdminSystem/DoctorDataEntry.aspx.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
+
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click1(object sender, EventArgs e)
+    {
+        //create an new instance of clsDoctor
+        clsDoctor ADoctor = new clsDoctor();
+        //capture the full name
+        ADoctor.FullName = txtFullName.Text;
+        //capture the password
+        ADoctor.Password = txtPassword.Text;
+        //capture the email
+        ADoctor.Email = txtEmail.Text;
+        //capture the department
+        ADoctor.Department = txtDepartment.Text;
+        //capture the contract date
+        ADoctor.ContractDate = Convert.ToDateTime(DateTime.Now);
+        //capture the availability
+        ADoctor.Available = chkAvailability.Checked;
+        //get the data from the session object
+        Session["ADoctor"] = ADoctor;
+        //navigate to the view page
+        Response.Redirect("DoctorViewer.aspx");
     }
 }
