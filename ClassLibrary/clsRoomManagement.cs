5711dd574d874e3e422f30f21f09e5563e1c8e67
Louise-Tebbutt <louiset05@outlook.com>
2026-05-07 16:55:20 +0100

    practical 7 compelte

--- ClassLibrary/clsRoomManagement.cs
+++ ClassLibrary/clsRoomManagement.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsRoomManagement
     {
+        public DateTime RoomEntryDate { get; set; }
+        public bool TreatmentOver { get; set; }
+        public int RoomID { get; set; }
+        public int PatientID { get; set; }
+        public string RoomName { get; set; }
+        public string ClinicalNotes { get; set; }
     }
 }
