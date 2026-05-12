93d16398f860a1f67ae9b72dfb57ad9dd1d16938
zozoapple <zoegracedickson@gmail.com>
2026-05-12 12:39:22 +0100

    Redid practical 2

--- ClassLibrary/clsMedicalRecord.cs
+++ ClassLibrary/clsMedicalRecord.cs
@@ -4,12 +4,13 @@ namespace ClassLibrary
 {
     public class clsMedicalRecord
     {
+     
         public DateTime DateAdded { get; set; }
         public int IdAdded { get; set; }
         public string MedsAdded { get; set; }
         public string DiagsAdded { get; set; }
         public bool SexAdded { get; set; }
         public string NotesAdded { get; set; }
-        public string PrescriptionsAdded { get; set; }
+
     }
 }
