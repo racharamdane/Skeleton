97ea44e315549737756a668b2e57042b35a05eaf
p2785371 <p2785371@my365.dmu.ac.uk>
2026-05-12 13:57:57 +0100

    finished practical 9

--- ClassLibrary/clsRoomManagement.cs
+++ ClassLibrary/clsRoomManagement.cs
@@ -5,10 +5,40 @@ namespace ClassLibrary
     public class clsRoomManagement
     {
         //private data meber for the room id property
-        
-        public DateTime RoomEntryDate { get; set; }
-        public bool TreatmentOver { get; set; }
-        public Int32 mRoomID
+        private Int32 mRoomID;
+        private bool mTreatmentOver;
+        private DateTime mRoomEntryDate;
+        private Int32 mPatientID;
+        private string mRoomName;
+        private string mClinicalNotes;
+
+        public DateTime RoomEntryDate
+        {
+            get
+            {
+               //this line of code sends data out of the property
+                return mRoomEntryDate;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mRoomEntryDate = value;
+            }
+        }
+        public bool TreatmentOver
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mTreatmentOver;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mTreatmentOver = value;
+            }
+        }
+        public Int32 RoomID
         {
             get
             {
@@ -21,12 +51,56 @@ namespace ClassLibrary
                 mRoomID = value;
             }
         }
-        public int PatientID { get; set; }
-        public string RoomName { get; set; }
-        public string ClinicalNotes { get; set; }
+        public int PatientID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mPatientID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mPatientID = value;
+            }
+        }
+        public string RoomName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mRoomName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mRoomName = value;
+            }
+        }
+        public string ClinicalNotes
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mClinicalNotes;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mClinicalNotes = value;
+            }
+        }
 
         public bool Find(int roomID)
         {
+            //set the private data members to the test data value
+            mRoomID = 1;
+            mTreatmentOver = false;
+            mRoomEntryDate = Convert.ToDateTime("11/05/2026");
+            mPatientID = 1;
+            mRoomName = "ObiWan";
+            mClinicalNotes = "Patient is doing well";
+
             //always return true
             return true;
         }
--- Testing4/tstRoomManagement.cs
+++ Testing4/tstRoomManagement.cs
@@ -130,5 +130,115 @@ namespace Testing4
             //test to see that the result is correct
             Assert.IsTrue(OK);
         }
+
+        [TestMethod]
+        public void TestTreatmentOverFound()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagement AnRoomManagement = new clsRoomManagement();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 RoomID = 1;
+            //invoke the method
+            Found = AnRoomManagement.Find(RoomID);
+            //check the room id
+            if (AnRoomManagement.TreatmentOver != false)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestRoomEntryDateFound()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagement AnRoomManagement = new clsRoomManagement();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 RoomID = 1;
+            //invoke the method
+            Found = AnRoomManagement.Find(RoomID);
+            //check the room id
+            if (AnRoomManagement.RoomEntryDate != Convert.ToDateTime("11/05/2026"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPatientIDFound()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagement AnRoomManagement = new clsRoomManagement();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 RoomID = 1;
+            //invoke the method
+            Found = AnRoomManagement.Find(RoomID);
+            //check the room id
+            if (AnRoomManagement.PatientID != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestRoomNameFound()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagement AnRoomManagement = new clsRoomManagement();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 RoomID = 1;
+            //invoke the method
+            Found = AnRoomManagement.Find(RoomID);
+            //check the room id
+            if (AnRoomManagement.RoomName != "ObiWan")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestClinicalNotesFound()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagement AnRoomManagement = new clsRoomManagement();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 RoomID = 1;
+            //invoke the method
+            Found = AnRoomManagement.Find(RoomID);
+            //check the room id
+            if (AnRoomManagement.ClinicalNotes != "Patient is doing well")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
     }
 }
