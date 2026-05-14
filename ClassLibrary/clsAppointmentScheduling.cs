c3f3401efcdb97cecefd9e0507d9750d1b3a0811
Zuby <P2850114@ERWX102AWIN305W>
2026-05-14 23:08:54 +0100

    Practical 9a Completed

---
+++ ClassLibrary/clsAppointmentScheduling.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class clsAppointmentScheduling
+    {
+        private Boolean mAvailability;
+        public bool Availability
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAvailability;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mAvailability = value;
+            }
+        }
+
+        private DateTime mAppointmentDate;
+        public DateTime AppointmentDate
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAppointmentDate;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mAppointmentDate = value;
+            }
+        }
+        private DateTime mAppointmentTime;
+        public DateTime AppointmentTime
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAppointmentTime;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mAppointmentTime = value;
+            }
+        }
+
+
+        private int mAppointmentId;
+        public int AppointmentId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAppointmentId;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mAppointmentId = value;
+            }
+
+        }
+        private int mDoctorId;
+        public int DoctorId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDoctorId;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mDoctorId = value;
+            }
+
+        }
+        private int mPatientId;
+        public int PatientId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mPatientId;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mPatientId = value;
+            }
+
+        }
+        private string mAppointmentNotes;
+        public string AppointmentNotes
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAppointmentNotes;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mAppointmentNotes = value;
+            }
+
+        }
+        /****** FIND METHOD ******/
+
+
+        public bool Find(int patientId)
+        {
+            // create an instance of the data connection 
+            clsDataConnection DB = new clsDataConnection();
+            // add the parameter for the patient id to search for
+            DB.AddParameter("@AppointmentId", AppointmentId);
+            // execute the stored procedure
+            DB.Execute("sproc_tblPatientManagement_FilterByAppointmentId");
+            //If one record is found (there should be either one or zero)
+            if (DB.Count == 1)
+            {
+                // copy the data from the database to the private data members 
+                mAppointmentId = Convert.ToInt32(DB.DataTable.Rows[0]["AppointmentId"]);
+                mDoctorId = Convert.ToInt32(DB.DataTable.Rows[0]["DoctorId"]);
+                mPatientId = Convert.ToInt32(DB.DataTable.Rows[0]["PatientId"]);
+                mAppointmentNotes = Convert.ToString(DB.DataTable.Rows[0]["Appointment Notes"]);
+                mAvailability = Convert.ToBoolean(DB.DataTable.Rows[0]["Availability"]);
+                mAppointmentDate = Convert.ToDateTime(DB.DataTable.Rows[0]["Appointment Date"]);
+                mAppointmentTime = Convert.ToDateTime(DB.DataTable.Rows[0]["Appointment Time"]);
+                // return that everything worked OK
+                return true;
+            }
+            // if no record is found
+            else
+            {
+                // return false indicating a problem
+                return false;
+            }
+        }
+    }
+}
---
+++ Testing3/tstAppointmentScheduling.cs
@@ -0,0 +1,274 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstAppointmentScheduling
+    {
+        private object DoctorId;
+        private object PatientId;
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //test to see that it exists
+            Assert.IsNotNull(AAppointmentScheduling);
+        }
+
+        [TestMethod]
+        public void AvailabilityOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AAppointmentScheduling.Availability = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AAppointmentScheduling.Availability, TestData);
+        }
+
+        [TestMethod]
+        public void AppointmentDateOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AAppointmentScheduling.AppointmentDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AAppointmentScheduling.AppointmentDate, TestData);
+        }
+
+        [TestMethod]
+        public void DoctorIdOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            Int32 TestData = 4;
+            //assign the data to the property
+            AAppointmentScheduling.DoctorId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AAppointmentScheduling.DoctorId, TestData);
+        }
+
+        [TestMethod]
+        public void PatientIdOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            Int32 TestData = 111;
+            //assign the data to the property
+            AAppointmentScheduling.PatientId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AAppointmentScheduling.PatientId, TestData);
+        }
+
+        [TestMethod]
+        public void AppointmentNotesOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            string TestData = "N/A";
+            //assign the data to the property
+            AAppointmentScheduling.AppointmentNote = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AAppointmentScheduling.AppointmentNote, TestData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create some test data to assign to the property
+            Boolean Found = false;
+            //assign the data to the property
+            int AppointmentId = 1234;
+            //test to see that the two values are the same
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestDoctorIdFound(object doctorId)
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(DoctorId);
+            //check the doctor Id
+            if (AAppointmentScheduling.DoctorId != 1)
+            {
+                OK = false;
+            }
+            //check to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestPatientIdFound(object patientId, object patientId1)
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(PatientId);
+            //check the doctor Id
+            if (AAppointmentScheduling.PatientId != 111)
+            {
+                OK = false;
+            }
+            //check to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestAppointmentIdFound()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            //check the doctor Id
+            if (AAppointmentScheduling.AppointmentId != 1234)
+            {
+                OK = false;
+            }
+            //check to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAppointmentNotesFound()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            //check the Full Name property
+            if (AAppointmentScheduling.AppointmentNotes != "N/A")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAppointmentDateFound()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            //check the doctor Id
+            if (AAppointmentScheduling.AppointmentDate != Convert.ToDateTime("29/04/2026"))
+            {
+                OK = false;
+            }
+            //check to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestAppointmentTimeFound()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            //check the doctor Id
+            if (AAppointmentScheduling.AppointmentTime != Convert.ToDateTime("16:00:00"))
+            {
+                OK = false;
+            }
+            //check to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAvailabilityFound()
+        {
+            //create an instance of the class we want to create
+            ClsAppointmentScheduling AAppointmentScheduling = new ClsAppointmentScheduling();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int AppointmentId = 1234;
+            //invoke the method
+            Found = AAppointmentScheduling.Find(AppointmentId);
+            //check the Full Name property
+            if (AAppointmentScheduling.Availability != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+    }
+
+    public class ClsAppointmentScheduling
+    {
+        internal DateTime AppointmentTime;
+        internal DateTime AppointmentDate;
+        internal string AppointmentNotes;
+        internal int PatientId;
+        internal bool Availability;
+        internal string AppointmentNote;
+        internal int AppointmentId;
+        internal int DoctorId;
+
+        internal bool Find(object appointmentSchedulingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+
