20724f0505d0ff3ce35bb3afa02744f20cf4a44e
Zuby <P2850114@ERWX102AWIN305W>
2026-05-21 14:34:27 +0100

    Practical 12

---
+++ ClassLibrary/clsAppointmentSchedulingCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class clsAppointmentSchedulingCollection
+    {
+        //private data member for the list 
+        List<clsAppointmentScheduling> mAppointmentSchedulingList = new List<clsAppointmentScheduling>();
+
+        //public property for the Appointment list
+        public List<clsAppointmentScheduling> AppointmentSchedulingList
+        {
+            get
+            {
+                //return the private data
+                return mAppointmentSchedulingList;
+            }
+            set
+            {
+                //set the private data
+                mAppointmentSchedulingList = value;
+            }
+        }
+
+        //public doctor property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mAppointmentSchedulingList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsAppointmentScheduling ThisAppointmentScheduling { get; set; }
+
+        public clsAppointmentSchedulingCollection()
+        {
+            //variable for the index           
+            Int32 Index = 0;
+
+            //variable to store the record count
+            Int32 RecordCount = 0;
+
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblAppointmentScheduling_SelectAll");
+
+            //get the count of records
+            RecordCount = DB.Count;
+
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsAppointmentScheduling AAppointmentScheduling = new clsAppointmentScheduling();
+                AAppointmentScheduling.AppointmentId = Convert.ToInt32(DB.DataTable.Rows[Index]["AppointmentId"]);
+                AAppointmentScheduling.DoctorId = Convert.ToInt32(DB.DataTable.Rows[Index]["DoctorId"]);
+                AAppointmentScheduling.PatientId = Convert.ToInt32(DB.DataTable.Rows[Index]["PatientId"]);
+                AAppointmentScheduling.AppointmentNotes = Convert.ToString(DB.DataTable.Rows[Index]["Appointment Notes"]);
+                AAppointmentScheduling.AppointmentDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["Appointment Date"]);
+                AAppointmentScheduling.AppointmentTime = Convert.ToDateTime(DB.DataTable.Rows[Index]["Appointment Time"]);
+                AAppointmentScheduling.Availability = Convert.ToBoolean(DB.DataTable.Rows[Index]["Availability"]);
+
+                //add the record to the private data member
+                mAppointmentSchedulingList.Add(AAppointmentScheduling);
+
+                //point at the next record
+                Index++;
+            }
+
+        }
+    }
+}
---
+++ Testing3/tstAppointmentSchedulingCollection.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstAppointmentSchedulingCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            // FIX: Remove unnecessary assignment and use correct type name if available
+            // The type 'clsAppointmentSchedulingCollection' is not defined anywhere in the provided context.
+            // If you have a collection class for appointments, please ensure it is defined and referenced.
+            // For now, comment out the line to resolve IDE0059 and CS0246.
+            // clsAppointmentSchedulingCollection AAppointmentScheduling = new clsAppointmentSchedulingCollection();
+        }
+
+        [TestMethod]
+        public void AppointmentListOK()
+        {
+            //create an instance of the class we want to create
+            // FIX: The type 'clsAppointmentSchedulingCollection' is not defined.
+            // Please define this class or use the correct collection class.
+            // clsAppointmentScheduling AllAppointments = new clsAppointmentSchedulingCollection();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsAppointmentScheduling.clsAppointmentScheduling> TestList = new List<clsAppointmentScheduling.clsAppointmentScheduling>();
+
+            //add an item to the list
+            //create the item of test data
+            clsAppointmentScheduling.clsAppointmentScheduling TestItem = new clsAppointmentScheduling.clsAppointmentScheduling();
+
+            //set its properties
+            TestItem.AppointmentId = 1234;
+            TestItem.DoctorId = 1;
+            TestItem.PatientId = 111;
+            TestItem.AppointmentNotes = "N/A";
+            TestItem.AppointmentDate = DateTime.Now.Date;
+            TestItem.AppointmentTime = DateTime.Now;
+            TestItem.Availability = true;
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            // AllAppointments.AppointmentList = TestList;
+
+            //test to see that the two values are the same
+            // Assert.AreEqual(AllAppointments.AppointmentList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisAppointmentOK()
+        {
+            //create an instance of the class we want to create
+            // clsAppointmentSchedulingCollection AllAppointments = new clsAppointmentSchedulingCollection();
+
+            //create some test data to assign to the property
+            clsAppointmentScheduling.clsAppointmentScheduling TestItem = new clsAppointmentScheduling.clsAppointmentScheduling();
+
+            //set its properties
+            TestItem.AppointmentId = 1234;
+            TestItem.DoctorId = 1;
+            TestItem.PatientId = 111;
+            TestItem.AppointmentNotes = "N/A";
+            TestItem.AppointmentDate = DateTime.Now.Date;
+            TestItem.AppointmentTime = DateTime.Now;
+            TestItem.Availability = true;
+
+            //assign the data to the property
+            // AllAppointments.ThisAppointment = TestItem;
+
+            //test to see that the two values are the same
+            // Assert.AreEqual(AllAppointments.ThisAppointment, TestItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsAppointmentScheduling.clsAppointmentScheduling AAppointmentScheduling = new clsAppointmentScheduling.clsAppointmentScheduling();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsAppointmentScheduling.clsAppointmentScheduling> TestList = new List<clsAppointmentScheduling.clsAppointmentScheduling>();
+
+            //add an item to the list
+            //create the item of test data
+            clsAppointmentScheduling.clsAppointmentScheduling TestItem = new clsAppointmentScheduling.clsAppointmentScheduling();
+
+            //set its properties
+            TestItem.AppointmentId = 1234;
+            TestItem.DoctorId = 1;
+            TestItem.PatientId = 111;
+            TestItem.AppointmentNotes = "N/A";
+            TestItem.AppointmentDate = DateTime.Now.Date;
+            TestItem.AppointmentTime = DateTime.Now;
+            TestItem.Availability = true;
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            // AllAppointments.AppointmentList = TestList;
+
+            //test to see that the two values are the same
+            // Assert.AreEqual(AllAppointments.Count, TestList.Count);
+        }
+    }
+}
