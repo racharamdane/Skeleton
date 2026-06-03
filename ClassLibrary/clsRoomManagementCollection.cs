67ccfa656158b3409e84c9d277a3cd6dfb125fa7
p2785371 <p2785371@my365.dmu.ac.uk>
2026-06-03 21:30:43 +0100

    practical 12 stuff

---
+++ ClassLibrary/clsRoomManagementCollection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ClassLibrary
+{
+    public class clsRoomManagementCollection
+    {
+        //constructor for the class
+        public clsRoomManagementCollection()
+        {
+            //create the items of test data
+            clsRoomManagement TestItem = new clsRoomManagement();
+            //set its properties
+            TestItem.RoomID = 1;
+            TestItem.RoomName = "Test Room";
+            TestItem.RoomEntryDate = DateTime.Now.Date;
+            TestItem.TreatmentOver = false;
+            TestItem.PatientID = 1;
+            TestItem.ClinicalNotes = "wrbxteyanlpofjibgbgzjczsuriqmgwdxlndgarfhihexxghhfcxnlcmskcueolxgkutzvcpytydqykykfntydirnqckfqtiwezop";
+            //add the item to the test list
+            mRoomList.Add(TestItem);
+            //re intialise 
+        }
+        //private data member for the list
+        List<clsRoomManagement> mRoomList = new List<clsRoomManagement>();
+        //public property for the room list
+        public List<clsRoomManagement> RoomList
+        {
+            get
+            {
+                //return the private data
+                return mRoomList;
+            }
+            set
+            {
+                //set the private data
+                mRoomList = value;
+            }
+        }
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mRoomList.Count;
+            }
+            set
+            {
+                //we will worry about this later
+            }
+        }
+        public clsRoomManagement ThisRoom { get; set; }
+    }
+}
---
+++ Testing4/tstRoomManagementCollection.cs
@@ -0,0 +1,112 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstRoomManagementCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllRooms);
+        }
+
+        [TestMethod]
+        public void RoomListOK()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsRoomManagement> TestList = new List<clsRoomManagement>();
+            //add an item to the list
+            //create the item of test data
+            clsRoomManagement TestItem = new clsRoomManagement();
+            //set its properties
+            TestItem.RoomID = 1;
+            TestItem.RoomName = "Test Room";
+            TestItem.RoomEntryDate = DateTime.Now.Date;
+            TestItem.TreatmentOver = false;
+            TestItem.PatientID = 1;
+            TestItem.ClinicalNotes = "wrbxteyanlpofjibgbgzjczsuriqmgwdxlndgarfhihexxghhfcxnlcmskcueolxgkutzvcpytydqykykfntydirnqckfqtiwezop";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllRooms.RoomList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllRooms.RoomList, TestList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instnace of the class wwe want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 0;
+            //assign the data to the property
+            AllRooms.Count = SomeCount;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllRooms.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisRoomPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            //create some test data to assign to the property
+            clsRoomManagement TestRoom = new clsRoomManagement();
+            //set the properties of the test object
+            TestRoom.RoomID = 1;
+            TestRoom.RoomName = "Test Room";
+            TestRoom.RoomEntryDate = DateTime.Now.Date;
+            TestRoom.TreatmentOver = false;
+            TestRoom.PatientID = 1;
+            TestRoom.ClinicalNotes = "wrbxteyanlpofjibgbgzjczsuriqmgwdxlndgarfhihexxghhfcxnlcmskcueolxgkutzvcpytydqykykfntydirnqckfqtiwezop";
+            //assign the data to the property
+            AllRooms.ThisRoom = TestRoom;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllRooms.ThisRoom, TestRoom);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            //create some test data to assign to the property
+            List<clsRoomManagement> TestList = new List<clsRoomManagement>();
+            //add an item to the list
+            //create the item of test data
+            clsRoomManagement TestItem = new clsRoomManagement();
+            //set its properties
+            TestItem.RoomID = 1;
+            TestItem.RoomName = "Test Room";
+            TestItem.RoomEntryDate = DateTime.Now.Date;
+            TestItem.TreatmentOver = false;
+            TestItem.PatientID = 1;
+            TestItem.ClinicalNotes = "wrbxteyanlpofjibgbgzjczsuriqmgwdxlndgarfhihexxghhfcxnlcmskcueolxgkutzvcpytydqykykfntydirnqckfqtiwezop";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllRooms.RoomList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllRooms.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class we want to create
+            clsRoomManagementCollection AllRooms = new clsRoomManagementCollection();
+            Assert.AreEqual(AllRooms.Count, 2);
+        }
+    }
+}
